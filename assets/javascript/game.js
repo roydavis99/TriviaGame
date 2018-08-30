cad9f7c4194f86afe5d56252c252722a3ea0741d
Rex <roydavis99.rd@gmail.com>
2018-08-29 23:19:17 -0700

    basic gameplay set up. with API calls

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,63 @@
+//Variables
+var wins = 0;
+var loss = 0;
+
+//states = ["loading","ready","guessing","selected"]
+var currentState = 0;
+
+
+
 $(document).ready(function(){
-console.log("here");
-LoadQuestion();
-console.log(questions[4]);
-//LoadDisplay(questions[4]);
+    checkState();
+
+$(".answer").click(function(){
+    if(currentState !== 2){return;}
+
+    console.log(this);
+    $(this).addClass("selected");
+    if($(this).attr("value")){
+        wins++;
+    }
+    else{
+        loss++;
+    }
+    setState(3);
 
 });
+});
+
+function setState(state){
+    console.log("set state: " + state);
+    currentState = state;
+    checkState();
+}
+
+function checkState(){
+    console.log("current state: " + currentState);
+
+    switch(currentState){
+        case 0:
+            $("#answer-container").addClass("hidden");
+            ResetAnswerState();
+        break;
+        case 1:
+            LoadQuestionDisplay(questions[currentQuestion]);
+            $("#answer-container").removeClass("hidden");
+        break;
+        case 2:
+            //guessing
+        break;
+        case 3:
+        setTimeout(function() {
+            DisplayCorrectAnswer();
+
+            setTimeout(function() {
+              setState(0);
+              currentQuestion++;
+              setState(1); 
+            }, 3000); 
+          }, 1000); 
+          
+        break;
+    }
+}
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,7 +1,8 @@
 var questions = [];
 var topics = ["all","videogames"];
+var currentQuestion = 0
 
-function LoadQuestion(topic){
+function LoadQuestions(topic){
     console.log("here");
     //Asyncrinis Javascritp xml
     $.ajax({
@@ -9,13 +10,60 @@ function LoadQuestion(topic){
         method: "GET"
     }).then(function(response){
         questions = response.results;
-        console.log(questions);
-        
-        LoadDisplay(questions[4]);
+        //console.log(questions);
+        setState(1);
+        //LoadQuestionDisplay(questions[4]);
     });
 }
+LoadQuestions("t"); // load API right away
 
-function LoadDisplay(quest){
+function LoadQuestionDisplay(quest){
     console.log(quest);
     $("#question").text(quest.question);
+    LoadAnswersRandom(quest);
+    setState(2);// currentState = 2;
+}
+
+function LoadAnswersRandom(question){
+    let rand = Math.floor(Math.random() * 4 ) +1;
+    $("#answer" + rand).text(question.correct_answer);
+    $("#answer" + rand).attr("value", true);
+
+    let j = 0;
+    for(let i = 1; i<=4; i++){
+        if(i !== rand){
+            $("#answer" + i).text(question.incorrect_answers[j]);
+            $("#answer" + i).attr("value", false); 
+            j++;
+        }
+    }
+
+}
+
+function DisplayCorrectAnswer(){
+    for(let i = 1; i<=4; i++){
+        console.log(i +" is: " + $("#answer" + i).attr("value"))
+        if($("#answer" + i).attr("value")=== "true"){
+            console.log(i +" is: " + $("#answer" + i).attr("value"))
+            $("#answer" + i).addClass("correct");
+        }
+        else if($("#answer" +i).hasClass("selected")){
+            console.log($("#answer" +i).hasClass("selected"));
+            $("#answer" + i).addClass("wrong");
+        }
+    }
+}
+
+function ResetAnswerState(){
+    for(let i = 1; i<=4; i++){
+        if($("#answer" +i).hasClass("selected")){
+            $("#answer" +i).removeClass("selected");
+        }
+        if($("#answer" +i).hasClass("wrong")){
+            $("#answer" +i).removeClass("wrong");
+        }
+        if($("#answer" +i).hasClass("correct")){
+            $("#answer" +i).removeClass("correct");
+        }
+    }
 }
